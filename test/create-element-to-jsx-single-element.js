968a6e89fbddf073555a29a6ea019cd413eabe1e
Eli White <github@eli-white.com>
2015-10-29 00:20:19 -0700

    Adding a codemod for transforming createElement function calls to JSX

---
+++ test/create-element-to-jsx-single-element.js
@@ -0,0 +1,4 @@
+var React = require('React');
+
+var a = React.createElement(Foo, null);
+var b = React.createElement('div', null);
---
+++ test/create-element-to-jsx-spread.output.js
@@ -0,0 +1,3 @@
+var React = require('React');
+
+<Foo foo="bar" {...someObject} />;
