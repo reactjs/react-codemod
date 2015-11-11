8d7ec2d6ceeb5b041fb9c407f025191ede306417
Daniel Howard <daniel.howard@nfl.com>
2015-11-11 12:44:37 -0800

    Add comments to test

--- test/convert-to-radium-test.output.js
+++ test/convert-to-radium-test.output.js
@@ -4,17 +4,25 @@ class Article extends React.Component {
   render() {
     return (
       <div>
+        // string literal classNames stay as classNames
         <span className="icon-class"></span>
         <span className="icon-class"></span>
         <span className="icon-class"></span>
         <span style={{width:'10px'}} className="icon-class"></span>
-        <span style={Object.assign({}, styles.one, {width:'10px'})}></span>
+
+        // js objects will get converted to style tags
+        // and merged with Object.assign if necessary.
+        // Calls to the classNames module will be stripped out
         <span style={styles.one}></span>
         <span style={Object.assign({}, styles.one, styles.two)}></span>
+        <span style={Object.assign({}, styles.one, {width:'10px'})}></span>
         <span
           style={Object.assign({}, styles.one, styles.two)}
           className="icon other-icon"></span>
-        <span className={classNames('hey', {'someStyle': true})}></span> // must be updated manually
+
+        // conditional styles in a classNames call cannot be
+        // migrated programmatically, will stay unchanged
+        <span className={classNames('hey', {'someStyle': booleanValue})}></span>
       </div>
     );
   }
