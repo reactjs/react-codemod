7f512f3e8d129332a43b98440d4217fbcb77e433
Robbie dela Victoria <robert.delavictoria@nfl.com>
2015-11-16 15:22:12 -0800

    updated scripts

--- test/__tests__/remove-gridiron-import-test.js
+++ test/__tests__/remove-gridiron-import-test.js
@@ -1,17 +1,9 @@
-/**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
 "use strict";
 
 describe("remove-gridiron-import", () => {
     it("transforms correctly", () => {
         test("remove-gridiron-import", "remove-gridiron-import");
+        //test("remove-gridiron-import", "remove-gridiron-import-2");
+        //test("remove-gridiron-import", "remove-gridiron-import-3");
     });
 });
---
+++ test/remove-gridiron-import-3.output.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+@component
+class DebugPanel extends React.Component {
+    render() {
+        return (
+            <div> test </div>
+        );
+    }
+}
+
+export default DebugPanel;
