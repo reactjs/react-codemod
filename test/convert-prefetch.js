5093f43c6753a1e11403b84764e3020e7ed2ebdd
Daniel Howard <daniel.howard@nfl.com>
2015-11-17 12:35:31 -0800

    Update test

--- test/convert-prefetch.js
+++ test/convert-prefetch.js
@@ -1,4 +1,4 @@
 import {Helmet} from "@nfl/gridiron";
-import {Prefetch} from "@nfl/gridiron";
+import {Prefetch} from "@nfl/gridiron/addons";
 
-import {Prefetch, Helmet} from "@nfl/gridiron";
+import {Prefetch, Helmet} from "@nfl/gridiron/addons";
--- test/convert-prefetch.output.js
+++ test/convert-prefetch.output.js
@@ -1,5 +1,5 @@
 import {Helmet} from "@nfl/gridiron";
 import Prefetch from "react-wildcat-prefetch";
 
-import {Helmet} from "@nfl/gridiron";
+import {Helmet} from "@nfl/gridiron/addons";
 import Prefetch from "react-wildcat-prefetch";
