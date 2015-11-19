7f4369fd965f94199f071a8a440df08ce8e8b534
Daniel Howard <daniel.howard@nfl.com>
2015-11-19 15:46:23 -0800

    Make sure rest of transforms work without styles

--- test/missing-styles.output.js
+++ test/missing-styles.output.js
@@ -1,15 +1,56 @@
+// this is a copy of convert-to-radium-test.js, except that styles won't beb imported
 import React from "react";
 import radium from "react-wildcat-radium";
 import styles from "styles/bad-styles.js";
 
+import component from "component";
+import fooBar from "fooBar";
+
+@component
+@fooBar({foo: "bar"})
 @radium
 class Article extends React.Component {
     render() {
         return (
             <div>
-                // if styles cannot be imported,
-                // no elements will receive a key attribute
+                // string literal classNames stay as classNames
+                <span className="icon-class"></span>
+                <span className="icon-class"></span>
+                <span className="icon-class"></span>
+                <span style={{width: "10px"}} className="icon-class"></span>
+
+                // js objects will get converted to style tags
+                // and merged into one object
+                // Calls to the classNames module will be stripped out
                 <span style={styles.one}></span>
+                <span style={styles.one}></span>
+                <span style={{
+                    ...styles.one,
+                    ...styles.two
+                }}></span>
+                <span
+                    style={{
+                        ...styles.one,
+                        ...{width: "10px"}
+                    }}></span>
+                <span
+                    style={{
+                        ...styles.one,
+                        ...styles.two
+                    }}
+                    className="icon other-icon"></span>
+
+                // nodes with interactive styles (hover state, media queries)
+                // don't get a "key" attribute added to them
+                <span style={styles.hover}></span>
+                <span style={styles.media1}></span>
+                <span style={styles.media2}></span>
+                <span style={styles.media3}></span>
+                <span style={styles.media4}></span>
+
+                // conditional styles in a classNames call cannot be
+                // migrated programmatically, will stay unchanged
+                <span className={classNames("icon", {"someStyle": booleanValue})}></span>
             </div>
         );
     }
