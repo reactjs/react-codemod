bb33b49a6eefccc55ae14ea78468712b2e8ed387
Daniel Howard <daniel.howard@nfl.com>
2015-11-19 13:56:33 -0800

    Log number of renamed files / Clear empty index list

--- src/codemod.js
+++ src/codemod.js
@@ -6,6 +6,7 @@ const fs = require("fs");
 const path = require("path");
 const _ = require("underscore");
 
+const emptyIndexFile = path.join(__dirname, "..", "empty_indexes.txt");
 const transformBasePath = path.join(__dirname, "..", "transforms");
 const runFirst = [
     "resolve-relative-imports.js"
@@ -63,14 +64,16 @@ const markForDeletion = function () {
 };
 
 const renameFiles = function () {
-    echo("Renaming files from .jsx to .js");
+    var files = find(opts.src)
+                    .filter(function (file) {
+                        return file.match(/\.jsx$/);
+                    });
 
-    find(opts.src)
-        .filter(function (file) {
-            return file.match(/\.jsx$/);
-        }).map(function (file) {
-            mv(file, file.replace("jsx", "js"));
-        });
+    echo("\nRenaming "+files.length+" files from .jsx to .js");
+
+    files.map(function (file) {
+        mv(file, file.replace("jsx", "js"));
+    });
 };
 
 const applyTransform = function (transforms) {
@@ -84,7 +87,7 @@ const applyTransform = function (transforms) {
 
     const cmd = buildCMD(transformFilePath, opts.src.replace(".jsx", ".js"));
 
-    echo("Applying transform", transformName);
+    echo("\nApplying transform", transformName);
 
     exec(cmd, function (err) {
         if (err) {
@@ -97,7 +100,7 @@ const applyTransform = function (transforms) {
 
 const deleteEmptyIndexes = function () {
     echo("Removing unused index files");
-    const emptyIndexFile = path.join(__dirname, "..", "empty_indexes.txt");
+
     const emptyIndexFiles = cat(emptyIndexFile);
 
     if (emptyIndexFiles === null) {
@@ -119,6 +122,7 @@ if (opts.clean) {
     deleteEmptyIndexes();
 }
 
+rm(emptyIndexFile);
 renameFiles();
 
 if (opts.all) {
