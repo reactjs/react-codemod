55de3643c3f346cbdf2f9c8b3f3423232a56d47e
Daniel Howard <daniel.howard@nfl.com>
2015-11-19 16:28:00 -0800

    Fix test

--- test/__tests__/convert-to-radium-test.js
+++ test/__tests__/convert-to-radium-test.js
@@ -3,16 +3,8 @@
 import p from "path";
 import sinon from "sinon";
 
-var errorArgs1 = [
-    "%s: Could not update styles on line %s",
-    "/Users/daniel.howard/Projects/react-codemod/test/convert-to-radium-test.js",
-    39
-];
-
-var errorArgs2 = [
-    "%s: Could not import styles, you will need to add key attributes manually to any elements with interactive styles",
-    "/Users/daniel.howard/Projects/react-codemod/test/convert-to-radium-test.js"
-];
+var errorArgs1 = ["%s: Could not update styles on line %s", null, 39];
+var errorArgs2 = ["%s: Could not import styles, you will need to add key attributes manually to any elements with interactive styles"];
 
 describe("convert-to-radium", () => {
     var logSpy = sinon.spy(console, "error");
@@ -21,9 +13,9 @@ describe("convert-to-radium", () => {
         const path = p.resolve(__dirname, "../convert-to-radium-test.js");
 
         test("convert-to-radium", "convert-to-radium-test", null, {path});
-        expect(console.error.calledWith(errorArgs1[0], errorArgs1[1], errorArgs1[2])).toEqual(true);
+        expect(console.error.calledWith(errorArgs1[0], sinon.match.any, errorArgs1[2])).toEqual(true);
 
         test("convert-to-radium", "missing-styles", null, {path});
-        expect(console.error.calledWith(errorArgs2[0], errorArgs2[1])).toEqual(true);
+        expect(console.error.calledWith(errorArgs2[0])).toEqual(true);
     });
 });
