5c3dac4b1616e3af386ea0092e10929d286fc50c
Richard Herrera <richard.herrera@nfl.com>
2015-11-21 13:52:32 -0800

    Convert all links to use a route helper.

---
+++ test/__tests__/convert-link-test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("convert-link", () => {
+    it("transforms correctly", () => {
+        test("convert-link", "convert-link");
+    });
+});
---
+++ test/convert-link.output.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {routeHelper as route} from "routes.config.js";
+import {Link} from "react-router";
+
+@component
+@fooBar({foo: "bar"})
+class NotFound extends React.Component {
+    static propTypes = {
+        isLoggedIn: React.PropTypes.bool.isRequired
+    }
+
+    render() {
+        return (
+            <div className="not-found">
+                <Link to={route.for(this.props.isLoggedIn ? "dashboard" : `/login?returnTo=/admin/dashboard`)}>
+                    You can go about your business
+                </Link>
+
+                <Link to={route.for("superbowl", linkParams)} style={styles.filterOption}>
+                    {game.romanNumeral}
+                </Link>
+
+                <Link
+                    style={linkedBrochureStyles.mobileLinkOverlay}
+                    to={route.for("guide", {id})} />
+
+                <Link
+                    to={route.for("dai-editor", {
+                        primaryId,
+                        backupId
+                    })}
+                    query={{
+                        ip: `38`
+                    }}>
+                    Primary {primaryId} {(backupId) ? `/ Backup ${backupId}` : null} / ip-address: 38
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
