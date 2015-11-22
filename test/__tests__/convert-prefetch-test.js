d1dc3507aeb77c1419ea71e1fcbba449015706d2
Richard Herrera <richard.herrera@nfl.com>
2015-11-22 13:09:55 -0800

    Add a test for when prefetch should not do anything.

--- test/__tests__/convert-prefetch-test.js
+++ test/__tests__/convert-prefetch-test.js
@@ -4,5 +4,6 @@ describe("convert-prefetch", () => {
     it("transforms correctly", () => {
         test("convert-prefetch", "convert-prefetch");
         test("convert-prefetch", "convert-prefetch-2");
+        test("convert-prefetch", "convert-prefetch-3");
     });
 });
---
+++ test/convert-prefetch-3.output.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {GridironComponent} from "@nfl/gridiron";
+import {Helmet} from "@nfl/gridiron/addons";
+
+class Home extends GridironComponent {
+    constructor(...args) {
+        super(...args);
+    }
+
+    render() {}
+}
+
+export default Home;
