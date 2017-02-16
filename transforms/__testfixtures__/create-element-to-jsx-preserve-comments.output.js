0e684b277039a025a1efbf45195281c2e69373f6
Alan Pierce <alangpierce@gmail.com>
2017-02-16 10:20:29 -0800

    Don't put comments on JSXIdentifier nodes in create-element-to-jsx (#96)
    * Don't put comments on JSXIdentifier nodes in create-element-to-jsx
    
    Previously, this code:
    
    ```js
    React.createElement(
      Foo, // This is a comment
      {bar: baz}
    );
    ```
    
    would change to this code:
    
    ```js
    <// This is a comment
    Foo bar={baz} />
    ```
    
    This is technically correct, but really ugly, so this commit changes the script
    to reposition the comment to be on the outer JSXElement instead of the
    JSXIdentifier directly. As we traverse the first arg to `React.createElement`,
    we collect all comments and add them to the array of comments that we'll
    eventually add to the outer JSXElement.
    
    Note that this depends on a fix to recast that was just released in version
    0.11.22. See this PR for more details: https://github.com/benjamn/recast/pull/364
    Without that change, the repositioned comment could sometimes cause the
    resulting code to be incorrect if it's at the start of the expression part of a
    return statement. The latest jscodeshift has recast 0.11.22 in its version
    range, so a fresh install makes everything work, but an old install might fail.
    
    * Update jscodeshift in package.json to try to get the build to work
    
    Also alphabetize the dependencies since that's what `npm install --save` does.
    
    * Update yarn.lock so recast gets updated
    
    This was done with `yarn remove jscodeshift` and `yarn add jscodeshift`, so it
    updated all jscodeshift dependencies to latest, including recast.

--- transforms/__testfixtures__/create-element-to-jsx-preserve-comments.output.js
+++ transforms/__testfixtures__/create-element-to-jsx-preserve-comments.output.js
@@ -1,34 +1,41 @@
 var React = require('react');
 
 const render = () => {
-  return /*1*//*4*//*2*//*3*/</*5*/div/*6*//*7*//*18*/
-    /*8*/className/*9*/=/*10*/"foo"/*11*/
-    /*12*/
-    /*13*///17
-    onClick/*14*/={/*15*/ this.handleClick}/*16*/>
-    {/*19*///25
-    </*20*/TodoList/*21*/./*22*/Item/*23*/ />/*24*/}
-    <span {.../*26*/getProps()/*27*/} />
-    <input /*28*//*29*/ />
-  </div>;
+  return (
+    /*1*//*4*//*2*//*3*/<div
+      /*8*/className/*9*/=/*10*/"foo"/*11*/
+      /*12*/
+      /*13*///17
+      onClick/*14*/={/*15*/ this.handleClick}/*16*/>
+      {/*19*///25
+      <TodoList.Item />/*24*//*20*//*23*//*21*//*22*//*20*//*23*//*21*//*22*//*21*//*22*/}
+      <span {.../*26*/getProps()/*27*/} />
+      {<input /> /*28*//*29*/}
+    </div>
+    /*5*//*6*//*7*//*18*/
+  );
 };
 
 const render2 = () => {
-  return <div
-    // Prop comment.
-    className="foo">
-    {// Child string comment.
-    'hello'}
-  </div>;
+  return (
+    <div
+      // Prop comment.
+      className="foo">
+      {// Child string comment.
+      'hello'}
+    </div>
+  );
 };
 
 const render3 = () => {
-  return <div>
-    {// Child element comment.
-    <span />}
-  </div>;
+  return (
+    <div>
+      {// Child element comment.
+      <span />}
+    </div>
+  );
 };
 
 const render4 = () => {
-  return <Foo/* No props to see here! */ />;
+  return <Foo />/* No props to see here! */;
 };
