086e2e210fad0d14e8a63a163300aef47615dde2
Jake Zatecky <jakezatecky@users.noreply.github.com>
2019-07-27 09:51:48 -0400

    Consistently use printOptions (#145)

--- transforms/React-PropTypes-to-prop-types.js
+++ transforms/React-PropTypes-to-prop-types.js
@@ -12,6 +12,7 @@
 
 module.exports = function(file, api, options) {
   const j = api.jscodeshift;
+  const printOptions = options.printOptions || { quote: 'single' };
   const root = j(file.source);
 
   const MODULE_NAME = options['module-name'] || 'prop-types';
@@ -291,6 +292,6 @@ module.exports = function(file, api, options) {
   }
 
   return hasModifications
-    ? root.toSource({ quote: 'single' })
+    ? root.toSource(printOptions)
     : null;
 };
