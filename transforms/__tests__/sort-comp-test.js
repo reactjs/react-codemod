e31280c85dabab50604128ad846f577755fcc8c3
Dmitry Korlas <dmitry.korlas@ringcentral.com>
2022-11-29 20:01:11 +0400

    move sort-comp fixtures to a separate folder

--- transforms/__tests__/sort-comp-test.js
+++ transforms/__tests__/sort-comp-test.js
@@ -2,14 +2,14 @@
  * Copyright 2015-present, Facebook, Inc.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
  *
  */
 
 'use strict';
 
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
-defineTest(__dirname, 'sort-comp');
-defineTest(__dirname, 'sort-comp', null, 'sort-comp2');
-defineTest(__dirname, 'sort-comp', null, 'sort-comp3');
-defineTest(__dirname, 'sort-comp', null, 'sort-comp-pure');
+defineTest(__dirname, 'sort-comp', null, 'sort-comp/sort-comp');
+defineTest(__dirname, 'sort-comp', null, 'sort-comp/sort-comp2');
+defineTest(__dirname, 'sort-comp', null, 'sort-comp/sort-comp3');
+defineTest(__dirname, 'sort-comp', null, 'sort-comp/sort-comp-pure');
